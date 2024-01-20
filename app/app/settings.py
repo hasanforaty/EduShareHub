56919c5e84f19e0caedcbc326300b967f01e0caf
hasan foraty <hasanforaty19@gmail.com>
2024-01-20 15:10:03 +0330

    install core app

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_spectacular',
-    'rest_framework'
+    'rest_framework',
+    'core',
 ]
 
 MIDDLEWARE = [
