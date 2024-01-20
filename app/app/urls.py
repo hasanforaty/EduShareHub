e428c42390d7e676ffd889d806ca6b581a82aa1c
hasan foraty <hasanforaty19@gmail.com>
2024-01-20 15:29:54 +0330

    linting and fix issue with workflow of GitHub Action

--- app/app/urls.py
+++ app/app/urls.py
@@ -24,5 +24,9 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui',)
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui',
+    )
 ]
